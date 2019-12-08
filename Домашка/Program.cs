15e97ee062fdea6b803ff6a98e8bb1164fac0e34
hammerhearted <nataran2001@mail.ru>
2019-12-09 00:03:42 +0500

    ,,,

--- Домашка/Program.cs
+++ Домашка/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace Домашка
+namespace Задача1Труъ
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var x = Input("x");
+            var y = Input("y");
+            Console.WriteLine("Значение выражения: " + LogicalExpression(x, y));
+            Console.ReadKey();
+        }
+        static bool LogicalExpression(int x, int y)
+        {
+            return (x < 20) ^ (y < 20);
+        }
+        static int Input(string name)
+        {
+            Console.WriteLine("Введите число " + name);
+            return int.Parse(Console.ReadLine());
         }
     }
 }
