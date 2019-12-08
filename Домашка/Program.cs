cd9408397300779595021a235ebb4696d740898e
hammerhearted <nataran2001@mail.ru>
2019-12-08 23:13:26 +0500

    0

--- Домашка/Program.cs
+++ Домашка/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-namespace Домашка
+namespace задание_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите адрес электронной почты");
+            string yourMail = Console.ReadLine();
+            int position = yourMail.IndexOf("@");
+            Console.WriteLine("Ваш почтовый ящик " + yourMail.Substring(0, position) + " находится на почтовом сервисе " + (yourMail.Substring(position + 1)));
+            Console.ReadKey();
+
         }
     }
 }
