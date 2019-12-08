fc2d1b4683b1aa6fad5d2423373d9073b7e96075
hammerhearted <nataran2001@mail.ru>
2019-12-08 21:04:06 +0500

    Добавьте файлы проекта.

---
+++ Домашка/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Домашка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
