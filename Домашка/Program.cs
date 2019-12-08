7fb2586133ee444fb9e23f29a7ce94047e61f8e1
hammerhearted <nataran2001@mail.ru>
2019-12-08 23:04:55 +0500

    ,,,

--- Домашка/Program.cs
+++ Домашка/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace Домашка
+namespace Трапеция
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("введите высоту");
+            int h = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите большее основание");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите меньшее основание");
+            int c = int.Parse(Console.ReadLine());
+            int s = h * (c + b) / 2;
+            Console.WriteLine("Площадь трапеции равна = ");
+            Console.WriteLine(s);
+            int a = (b - c) / 2;
+            double d = Math.Sqrt((a * a) - (h * h));
+            double p = (d * 2) + b + c;
+            Console.WriteLine("Периметр трапеции равен = ");
+            Console.WriteLine(p);
+            Console.ReadKey();
+
         }
     }
 }
+
