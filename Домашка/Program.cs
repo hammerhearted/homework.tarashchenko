dc780bb0b4b76fd69fa6bb317a66aa6a96a8b1cf
hammerhearted <nataran2001@mail.ru>
2019-12-08 21:08:41 +0500

    ,,,

--- Домашка/Program.cs
+++ Домашка/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace Домашка
+namespace Квадранты
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите значение Х");
+            var X = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение Y");
+            var Y = int.Parse(Console.ReadLine());
+            if (X > 0 & Y > 0)
+                Console.WriteLine("точка лежит в 1 квадранте");
+            else if (X > 0 & Y < 0)
+                Console.WriteLine("точка лежит в 2 квадранте");
+            else if (X < 0 & Y < 0)
+                Console.WriteLine("точка лежит в 3 квадранте");
+            else if (X < 0 & Y > 0)
+                Console.WriteLine("точка лежит в 4 квадранте");
+            else if (X != 0 & Y == 0)
+                Console.WriteLine("точка лежит на оси абсцисс");
+            else if (X == 0 & Y != 0)
+                Console.WriteLine("точка лежит на оси ординат");
+            else
+                Console.WriteLine("точка совпадает с началом координат");
+            Console.ReadKey();
         }
     }
 }
