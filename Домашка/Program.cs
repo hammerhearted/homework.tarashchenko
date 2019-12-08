18c26f44d7c0422e419780050e0d183b9a04aac7
hammerhearted <nataran2001@mail.ru>
2019-12-08 22:51:22 +0500

    ,,,

--- Домашка/Program.cs
+++ Домашка/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace Домашка
+namespace SinCos
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double angle1 = 15;
+            Console.WriteLine(Math.Round(Math.Sin(angle1), 3));
+            Console.WriteLine(Math.Round(Math.Cos(angle1), 3));
+            double angle2 = 37;
+            Console.WriteLine(Math.Round(Math.Sin(angle2), 3));
+            Console.WriteLine(Math.Round(Math.Cos(angle2), 3));
+            double angle3 = 113;
+            Console.WriteLine(Math.Round(Math.Sin(angle3), 3));
+            Console.WriteLine(Math.Round(Math.Cos(angle3), 3));
+            Console.WriteLine("Введите значение угла");
+            double angle4 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Round(Math.Sin(angle4), 3));
+            Console.WriteLine(Math.Round(Math.Cos(angle4), 3));
+
+            Console.ReadKey();
         }
+
     }
 }
+
