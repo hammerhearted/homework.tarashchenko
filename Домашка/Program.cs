c959d75b6f676859aa00a8a31f5024138d7ebb06
hammerhearted <nataran2001@mail.ru>
2019-12-08 23:50:07 +0500

    ,,,

--- Домашка/Program.cs
+++ Домашка/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace Домашка
+namespace График
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var x = Input("x");
+            var y = Input("y");
+
+            Console.WriteLine("Значение выражения: " + LogicalExpression(x, y));
+            Console.ReadKey();
+        }
+        static bool LogicalExpression(double y, double x)
+        {
+            return x < 1.5 & y > -2;
+        }
+        static double Input(string name)
+        {
+            Console.WriteLine("Введите координату " + name);
+            return double.Parse(Console.ReadLine());
         }
     }
 }
+
+
