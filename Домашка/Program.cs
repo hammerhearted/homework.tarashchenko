8076e3f0fe5f98a3378a9203e2066244da67ecea
hammerhearted <nataran2001@mail.ru>
2019-12-15 22:42:31 +0500

    ...

--- Домашка/Program.cs
+++ Домашка/Program.cs
@@ -8,15 +8,15 @@ namespace Трапеция
         {
 
             Console.WriteLine("введите высоту");
-            int h = int.Parse(Console.ReadLine());
+            double h = int.Parse(Console.ReadLine());
             Console.WriteLine("введите большее основание");
-            int b = int.Parse(Console.ReadLine());
+            double b = int.Parse(Console.ReadLine());
             Console.WriteLine("введите меньшее основание");
-            int c = int.Parse(Console.ReadLine());
-            int s = h * (c + b) / 2;
+            double c = int.Parse(Console.ReadLine());
+            double s = h * (c + b) / 2;
             Console.WriteLine("Площадь трапеции равна = ");
             Console.WriteLine(s);
-            int a = (b - c) / 2;
+            double a = (b - c) / 2;
             double d = Math.Sqrt((a * a) - (h * h));
             double p = (d * 2) + b + c;
             Console.WriteLine("Периметр трапеции равен = ");
