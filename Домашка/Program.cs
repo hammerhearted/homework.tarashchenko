21f8801a1c2701361e7538c84d4c737ec875580d
hammerhearted <nataran2001@mail.ru>
2019-12-15 23:21:49 +0500

    ...

--- Домашка/Program.cs
+++ Домашка/Program.cs
@@ -7,20 +7,20 @@ namespace Квадранты
         static void Main(string[] args)
         {
             Console.WriteLine("Введите значение Х");
-            var X = int.Parse(Console.ReadLine());
+            var x = double.Parse(Console.ReadLine());
             Console.WriteLine("Введите значение Y");
-            var Y = int.Parse(Console.ReadLine());
-            if (X > 0 & Y > 0)
+            var y = double.Parse(Console.ReadLine());
+            if (x > 0 & y > 0)
                 Console.WriteLine("точка лежит в 1 квадранте");
-            else if (X > 0 & Y < 0)
+            else if (x > 0 & y < 0)
                 Console.WriteLine("точка лежит в 2 квадранте");
-            else if (X < 0 & Y < 0)
+            else if (x < 0 & y < 0)
                 Console.WriteLine("точка лежит в 3 квадранте");
-            else if (X < 0 & Y > 0)
+            else if (x < 0 & y > 0)
                 Console.WriteLine("точка лежит в 4 квадранте");
-            else if (X != 0 & Y == 0)
+            else if (x != 0 & y == 0)
                 Console.WriteLine("точка лежит на оси абсцисс");
-            else if (X == 0 & Y != 0)
+            else if (x == 0 & y != 0)
                 Console.WriteLine("точка лежит на оси ординат");
             else
                 Console.WriteLine("точка совпадает с началом координат");
